2c8f3dfabbe241fbb0eb24707d12d4a7b4bb9cfb
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-20 16:19:22 +0200

    show Files.write/.writeString

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,42 @@
 package dev.lpa;
 
+import dev.lpa.student.Course;
+import dev.lpa.student.Student;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
   
   public static void main(String[] args) {
   
-  
+    String header = """
+      Student Id,Country Code,Enrolled Year,Age,Gender,\
+      Experienced,Course Code,Engagement Month,Engagement Year,\
+      Engagement Type""";
+    
+    Course jmc = new Course("JMC", "Java Masterclass");
+    Course pymc = new Course("PYC", "Python Masterclass");
+    List<Student> students = Stream
+                               .generate(() -> Student.getRandomStudent(jmc, pymc))
+                               .limit(5)
+                               .toList();
+    
+//    System.out.println(header);
+//    students.forEach(s -> s.getEngagementRecords().forEach(System.out::println));
+    
+    Path path = Path.of("students.csv");
+    try {
+      Files.writeString(path, header);
+      for (var student : students) {
+        Files.write(path, student.getEngagementRecords(), StandardOpenOption.APPEND);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
