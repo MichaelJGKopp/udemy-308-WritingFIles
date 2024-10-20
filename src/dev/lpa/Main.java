606132fb1fdd21a7b52a477330fad696cdf241be
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-10-20 16:27:26 +0200

    write list of strings at once to file avoiding opening/closing each time

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -6,14 +6,14 @@ import dev.lpa.student.Student;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
   
   public static void main(String[] args) {
-  
+    
     String header = """
       Student Id,Country Code,Enrolled Year,Age,Gender,\
       Experienced,Course Code,Engagement Month,Engagement Year,\
@@ -25,18 +25,29 @@ public class Main {
                                .generate(() -> Student.getRandomStudent(jmc, pymc))
                                .limit(5)
                                .toList();
-    
+
 //    System.out.println(header);
 //    students.forEach(s -> s.getEngagementRecords().forEach(System.out::println));
     
     Path path = Path.of("students.csv");
+//    try {
+//      Files.writeString(path, header);
+//      for (var student : students) {
+//        Files.write(path, student.getEngagementRecords(), StandardOpenOption.APPEND);
+//      }
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
     try {
-      Files.writeString(path, header);
+      List<String> data = new ArrayList<>();
+      data.add(header);
       for (var student : students) {
-        Files.write(path, student.getEngagementRecords(), StandardOpenOption.APPEND);
+        data.addAll(student.getEngagementRecords());
       }
+      Files.write(path, data);
     } catch (IOException e) {
       e.printStackTrace();
     }
+   
   }
 }
